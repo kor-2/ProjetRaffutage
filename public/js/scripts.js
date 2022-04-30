25857c859abce63bf080ad07c975aaa838a4a907
kor-2 <checkmann.corentin@gmail.com>
2022-04-30 17:43:59 +0200

    ajout calendar bundle

---
+++ public/js/scripts.js
@@ -0,0 +1,31 @@
+////////////////////////////////////////////
+// affichage calendrier dashboard
+/////////////////////////////////////////////
+
+let rdv = document.getElementById('rdvJs').dataset.choise
+console.log("rdv")
+    console.log(rdv)
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            locale: 'fr',
+            timeZone: 'Europe/Paris',
+            headerToolbar: {
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay',
+              },
+            //events: rdv,
+          });
+        calendar.render();
+        });
+
+//         const json = '{"result":true, "count":42}';
+// const obj = JSON.parse(json);
+
+// console.log(obj.count);
+// // expected output: 42
+
+// console.log(obj.result);
+// // expected output: true
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Prestation;
 use App\Entity\User;
+use App\Repository\PrestationRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -19,6 +22,30 @@ class DashboardController extends AbstractDashboardController
         return $this->render('admin/dashboard.html.twig');
     }
 
+    /**
+     * @Route("/admin/planning", name="admin_planning")
+     */
+    public function planning(PrestationRepository $presRepo): Response
+    {
+        $plans = $presRepo->findAll();
+        $rdvLibre = [];
+        foreach ($plans as $plan) {
+            $rdvLibre[] = [
+                'title' => 'Créneau libre',
+                'id' => $plan->getId(),
+                'start' => $plan->getDebut()->format('Y-m-d H:i:s'),
+                'end' => $plan->getFin()->format('Y-m-d H:i:s'),
+                'backgroundColor' => '#80ffaa',
+            ];
+        }
+        $data = json_encode($rdvLibre);
+        dump($data);
+
+        return $this->render('admin/planning.html.twig', [
+            'data' => $data,
+        ]);
+    }
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
@@ -29,8 +56,25 @@ class DashboardController extends AbstractDashboardController
     {
         return[
         MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
-
+        MenuItem::section('Planning'),
+        MenuItem::linkToCrud('Créneaux horaires ', 'fas fa-clock', Prestation::class),
+        MenuItem::linkToRoute('Planning', 'fas fa-calendar', 'admin_planning'),
+        MenuItem::section('Gestion entité'),
         MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class),
         ];
+    }
+
+    public function configureAssets(): Assets
+    {
+        $assets = Assets::new();
+
+        return $assets
+        // css pour CalendarBundle
+        ->addHtmlContentToHead('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.css" integrity="sha256-jLWPhwkAHq1rpueZOKALBno3eKP3m4IMB131kGhAlRQ=" crossorigin="anonymous">')
+        // js pour CalendarBundle
+        ->addHtmlContentToHead('<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.js" integrity="sha256-XCdgoNaBjzkUaEJiauEq+85q/xi/2D4NcB3ZHwAapoM=" crossorigin="anonymous"></script>')
+        ->addJsFile('js/scripts.js')
+        ->addCssFile('css/admin.css')
+        ;
     }
 }
